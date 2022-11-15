ec0b82022ec5da150e47eb80ad9372fd7dc051e1
musty1406 <musty1406@gmail.com>
2022-11-15 12:29:33 +0300

    first Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App flex justify-center">
+      <h1 className="bg-slate-600 hover:bg-red-600 text-white w-14 h-9 text-center">
+        Başlık
+      </h1>
     </div>
   );
 }
