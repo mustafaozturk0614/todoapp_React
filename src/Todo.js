b0e3bb5bcbec8fa5242bbf1dc3bc77e0c5ed945c
musty1406 <musty1406@gmail.com>
2022-11-17 13:59:58 +0300

    fetc api işlemleri yapıldı

--- src/Todo.js
+++ src/Todo.js
@@ -1,24 +1,82 @@
-import React from "react";
+import { data } from "autoprefixer";
+import React, { useEffect } from "react";
 import { useState } from "react";
+import Table from "./component/Table";
 function Todo() {
+  console.log("Todo");
   const [todos, setTodos] = useState([]);
-  const [content, setContent] = useState();
-  const [date, setDate] = useState();
+  // const [content, setContent] = useState();
+  // const [date, setDate] = useState();
+  const [todo, setTodo] = useState({
+    userId: 1,
+    id: 1,
+    title: "delectus aut autem",
+    completed: false,
+    date: null,
+  });
 
-  const onChangeContent = (e) => {
-    setContent(e.target.value);
+  // const onChangeContent = (e) => {
+  //   setContent(e.target.value);
+  // };
+  // const onChangeDate = (e) => {
+  //   setDate(e.target.value);
+  // };
+  const loadData = () => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setTodos(data);
+      })
+      .catch((error) => console.log(error));
   };
-  const onChangeDate = (e) => {
-    setDate(e.target.value);
+
+  useEffect(() => {
+    console.log("her renderda çalışır");
+  });
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  const oncahngeTodo = (e) => {
+    const { name, value } = e.target;
+
+    setTodo((t) => ({
+      ...t,
+      [name]: value,
+      state: false,
+      id: todos.length + 1,
+    }));
   };
+
+  // const addTodo = (e) => {
+  //   setTodos([
+  //     ...todos,
+  //     {
+  //       content: content,
+  //       date: date,
+  //     },
+  //   ]);
+  // };
   const addTodo = (e) => {
-    setTodos([
-      ...todos,
-      {
-        content: content,
-        date: date,
-      },
-    ]);
+    setTodos([...todos, todo]);
+  };
+  const delteTodo = (e) => {
+    // let x = todos.splice(index, 1);
+    let index = e.target.value;
+    let list = [...todos];
+    list.splice(index, 1);
+    setTodos(list);
+  };
+
+  const editTodo = (e) => {
+    let index = e.target.value;
+    let list = [...todos];
+    let newTodo = list[index];
+    newTodo.state = true;
+
+    setTodos([...list]);
   };
 
   return (
@@ -28,18 +86,18 @@ function Todo() {
         <form className="row flex-col mx-auto justify-center w-50 mt-5">
           <input
             type="text"
-            id="content"
-            name="contet"
+            id="title"
+            name="title"
             placeholder="Lütfen Bir Todo Giriniz"
             className="w-50 rounded-md border px-2 my-3 "
-            onChange={onChangeContent}
+            onChange={oncahngeTodo}
           ></input>
           <input
             type="date"
             id="date"
             name="date"
             className="w-auto border rounded-md"
-            onChange={onChangeDate}
+            onChange={oncahngeTodo}
           ></input>
 
           <button
@@ -54,37 +112,33 @@ function Todo() {
       </div>
 
       <div>
-        <table className="table">
-          <thead>
-            <tr>
-              <th scope="col">Sıra No</th>
-              <th scope="col">Todo</th>
-              <th scope="col">Tarih</th>
-              <th scope="col">Durum</th>
-              <th scope="col" className="text-center">
-                İşlemler
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {todos.map((data, index) => {
-              return (
-                <tr key={index}>
-                  <td>{index + 1}</td>
-                  <td>{data.content}</td>
-                  <td>{data.date}</td>
-                  <td>false</td>
-                  <td className="flex justify-center">
-                    <div className="text-center">
-                      <button className="btn btn-danger w-20 mx-1">Sil</button>
-                      <button className="btn btn-warning w-20 ">Düzenle</button>
-                    </div>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+        <Table
+          head={["Sıra No", "Yapılacklar", "Tarih", "Durum", "İşlemler"]}
+          body={todos.map((data, index) => [
+            data.id,
+            data.title,
+            data.date,
+            data.completed.toString(),
+            [
+              <button
+                value={index}
+                onClick={delteTodo}
+                className="btn btn-danger w-20 mx-1"
+              >
+                Sil
+              </button>,
+              <button
+                value={index}
+                onClick={editTodo}
+                className="btn btn-warning w-20 "
+              >
+                Düzenle
+              </button>,
+            ],
+          ])}
+          editTodo={editTodo}
+          delteTodo={delteTodo}
+        ></Table>
       </div>
     </div>
   );
